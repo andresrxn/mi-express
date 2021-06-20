738d33cba148c288c499a00b0ad38b27160ea0ce
andresrxn <77864035+andresrxn@users.noreply.github.com>
2021-06-20 16:53:29 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.getElementById("nav");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + 0.15}s`;
+            }
+
+        });
+
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
+
+}
+
+navSlide();
+
+let contactTitle = document.getElementById('contact-title');
+
+contactTitle.addEventListener('click', () => {
+    let contactPanel = document.getElementById('contact-panel');
+    contactPanel.classList.add('active');
+    let shadowPanel = document.getElementById('shadow-panel');
+    shadowPanel.classList.add('active');
+});
+
+
+
+
+const panelClose = document.getElementById('panel-close');
+
+panelClose.addEventListener('click', () => {
+    let contactPanel = document.getElementById('contact-panel');
+    contactPanel.classList.toggle('active');
+    let shadowPanel = document.getElementById('shadow-panel');
+    shadowPanel.classList.remove('active');
+});
+let shadowPanel = document.getElementById('shadow-panel');
+shadowPanel.addEventListener('click', () => {
+    let contactPanel = document.getElementById('contact-panel');
+    contactPanel.classList.toggle('active');
+    let shadowPanel = document.getElementById('shadow-panel');
+    shadowPanel.classList.remove('active');
+});
